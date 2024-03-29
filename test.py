66e3ed290553aaaac9728dea871013ad54592fe0
Laksri1 <egc.laksri@gmail.com>
2024-03-29 18:36:35 +0530

    with distance

--- test.py
+++ test.py
@@ -10,6 +10,8 @@ cfg.enable_stream(rs.stream.depth, 640,480, rs.format.z16, 30)
 
 pipe.start(cfg)
 
+point = (320,240)
+
 while True:
     frame = pipe.wait_for_frames()
     depth_frame = frame.get_depth_frame()
@@ -18,11 +20,14 @@ while True:
     depth_image = np.asanyarray(depth_frame.get_data())
     color_image = np.asanyarray(color_frame.get_data())
     depth_cm = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha = 0.5), cv2.COLORMAP_JET)
+    color_image = cv2.circle(color_image, point, 2, (0, 0, 255), -1) 
 
 
     cv2.imshow('rgb', color_image)
     # cv2.imshow('depth', depth_image)
     cv2.imshow('cm', depth_cm)
+
+    print(depth_frame.get_distance(320,240))
 
     if cv2.waitKey(1) == ord('q'):
         break
